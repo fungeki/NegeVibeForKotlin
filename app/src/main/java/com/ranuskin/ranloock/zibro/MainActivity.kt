a0e1cb4fd93e7244bcf29bc5c6eaaa5cfb3b6fad
fungeki <fang.society@gmail.com>
2019-01-22 23:28:04 +0200

    fragment to header

--- app/src/main/java/com/ranuskin/ranloock/zibro/MainActivity.kt
+++ app/src/main/java/com/ranuskin/ranloock/zibro/MainActivity.kt
@@ -2,15 +2,16 @@ package com.ranuskin.ranloock.zibro
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ranuskin.ranloock.zibro.Fragments.EventListFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -27,10 +28,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
+
+        bottom_nav_bar.selectedItemId = R.id.bot_nav_event_list
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+       // supportFragmentManager.beginTransaction().add(R.id.main_fragment_container, EventListFragment()).commit()
         nav_view.setNavigationItemSelectedListener(this)
+        bottom_nav_bar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        supportFragmentManager.beginTransaction().replace(R.id.fragments_container, EventListFragment()).commit()
     }
 
     override fun onBackPressed() {
@@ -43,18 +49,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+        return false
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
+        return false
+//        when (item.itemId) {
+//            R.id.action_settings -> return true
+//            else -> return super.onOptionsItemSelected(item)
+//        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -79,5 +87,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.bot_nav_event_list -> {
+                println("meow")
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_container, EventListFragment()).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
