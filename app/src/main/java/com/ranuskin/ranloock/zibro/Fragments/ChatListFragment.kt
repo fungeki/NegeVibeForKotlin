03d9315723d3439af6bf3ea2d26a7ffa284eccd2
fungeki <fang.society@gmail.com>
2019-01-31 23:10:41 +0200

    username select dialog upgrades

--- app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/ChatListFragment.kt
+++ app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/ChatListFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,7 @@ import com.ranuskin.ranloock.zibro.Adapters.ChatListAdapter
 import com.ranuskin.ranloock.zibro.R
 import kotlinx.android.synthetic.main.fragment_chat_list.*
 import kotlinx.android.synthetic.main.username_select_dialog.view.*
+import java.util.regex.Pattern
 
 
 /**
@@ -33,8 +36,8 @@ class ChatListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         chat_list_recycler_view.layoutManager = LinearLayoutManager(context)
         chat_list_recycler_view.adapter = ChatListAdapter{ event ->
-            print(event.title)
             showUsernameDialog()
+
         }
 
     }
@@ -47,14 +50,39 @@ class ChatListFragment : Fragment() {
 
         var dialog = builder.create()
         dialog.show()
+        dialogView.username_select_dialog_accept.isEnabled = false
         dialogView.username_select_dialog_accept.setOnClickListener {
 
         }
         dialogView.username_select_dialog_cancel.setOnClickListener {
             dialog.dismiss()
         }
+        dialogView.username_select_dialog_name_edittext.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let { str->
+                    dialogView.username_select_dialog_accept.isEnabled = str.length > 4 && isValidName(str.toString())
+                }
+            }
+
+        })
 
 
 
+    }
+
+    fun isValidName(str: String): Boolean{
+        val num = ".*[0-9a-zA-Zא-ת ].*"
+        val p = Pattern.compile(num)
+        val matcher = p.matcher(str)
+
+        return matcher.matches()
     }
 }
