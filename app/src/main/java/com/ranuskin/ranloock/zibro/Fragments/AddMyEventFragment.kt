0cb0487a5fa49db4458328db2be3c919e741e2ee
krai1181 <evgeniasimakov@gmail.com>
2019-02-07 14:33:38 +0200

    addEventFragment new Permission in Manifest

--- app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/AddMyEventFragment.kt
+++ app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/AddMyEventFragment.kt
@@ -7,24 +7,46 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 import com.ranuskin.ranloock.zibro.R
+import kotlinx.android.synthetic.main.fragment_add_my_event.*
 
 class AddMyEventFragment : Fragment() {
 
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Create an ArrayAdapter
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_my_event, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter.createFromResource(
+            view.context,
+            R.array.categoriesHebrew,
+            android.R.layout.simple_spinner_item
+        )
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        addEventCategorySpinner.adapter = adapter
+
+
+        addEventCategorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+        }
+
+
     }
 
 
