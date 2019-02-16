5d4244df31ebf325b7c4a1f5943632b3acd3966c
dortzemach <tzemachdor@gmail.com>
2019-02-17 00:21:49 +0200

    start of ManagementMyEventMenu

--- app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/ManagmentMyEventMenuFragment.kt
+++ app/src/main/java/com/ranuskin/ranloock/zibro/Fragments/ManagmentMyEventMenuFragment.kt
@@ -3,11 +3,15 @@ package com.ranuskin.ranloock.zibro.Fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ranuskin.ranloock.zibro.Adapters.MyEventsAdapter
 
 import com.ranuskin.ranloock.zibro.R
+import kotlinx.android.synthetic.main.fragment_managment_my_event_menu.*
+import kotlinx.android.synthetic.main.fragment_my_events_list.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +31,15 @@ class ManagmentMyEventMenuFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_managment_my_event_menu, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        managementMyEventMenuRecyclerView.layoutManager = LinearLayoutManager(context)
+
+
+    }
+//my_events_recyclerview.adapter = MyEventsAdapter(
+    //managementMyEventMenuRecyclerView
+    //managementMyEventMenuRecyclerView.layoutManager = LinearLayoutManager(context)
 
 
 }
